e6d7c04a24c2666f71e0ca094677c9ba7bbf65d1
Eric Wheeler <perl-mdf@z.ewheeler.org>
2022-06-20 17:09:16 -0700

    Added options that are useful to track as part of the RF::Component object.
        filename - original filename the component came from, if any
        model (derived from filename if not passed)
    
        value - like 10 (as in 10 pF)
        value_unit - unit, like pF
    
        Regular expressions to match against the model to extract the numerical component value:
            value_code_regex     3-digit component power format (123 means 12000)
            value_literal_regex  Literal number value (123 means 123)

--- lib/RF/Component.pm
+++ lib/RF/Component.pm
@@ -2,11 +2,24 @@ package RF::Component;
 
 use strict;
 use warnings;
+use Carp;
+use 5.010;
 
 use Math::Complex;
 use Math::Matrix::Complex;
 
-our %valid_opts = map { $_ => 1 } qw/measurements z n_ports comments/;
+our %valid_opts = map { $_ => 1 } qw/
+		measurements
+		z
+		n_ports
+		comments
+		filename
+		model
+		value
+		value_unit
+		value_code_regex
+		value_literal_regex
+		/;
 
 sub new
 {
@@ -14,10 +27,59 @@ sub new
 
 	foreach (keys %args)
 	{
-		die "$class: invalid class option: $_ => $args{$_}" if !defined($valid_opts{$_});
+		croak "$class: invalid class option: $_ => $args{$_}" if !defined($valid_opts{$_});
 	}
 
 	my $self = bless(\%args, $class);
+
+	if (defined($self->{value}) && ($self->{value_code_regex} || $self->{value_literal_regex}))
+	{
+		croak "Cannot define both value and value_(code|literal)_regex";
+	}
+
+	# Assume the part "model" is the filemodel without the .sNp suffix:
+	if (defined $self->{filename})
+	{
+		my $model = $self->{filename};
+		$model =~ s!^.*/|\.s\dp$!!ig;
+		$self->{model} //= $model;
+	}
+
+
+	if (defined($self->{model}) && $self->{value_code_regex} && !$self->{value_literal_regex})
+	{
+		$self->{value} = _parse_model_value_code($self->{model}, $self->{value_code_regex});
+	}
+	elsif (defined($self->{model}) && $self->{value_literal_regex} && !$self->{value_code_regex})
+	{
+		$self->{value} = _parse_model_value_literal($self->{model}, $self->{value_literal_regex});
+	}
+	elsif (!defined($self->{model}) && ($self->{value_literal_regex} || $self->{value_code_regex}))
+	{
+		croak "model number must be defined if you pass value_literal_regex or value_code_regex"
+	}
+	elsif (defined($self->{model}) && $self->{value_literal_regex} && $self->{value_code_regex})
+	{
+		croak "value_literal_regex and value_code_regex are mutually exclusive, use only one.";
+	}
+	else {} # model can be passed without a regex, but then value cannot be defined.
+
+
+	if (defined($self->{value}) && $self->{value} !~ /^\d+\.?\d*|\d*\.?\d+$/)
+	{
+		croak("component value is not numeric: $self->{value}");
+	}
+
+	if (defined($self->{value_unit}) && $self->{value_unit} !~ /^[fpnumkmGTPE]?[FHR]$/)
+	{
+		croak "invalid unit: expected sU where 's' is an si prefix (fpnumkmGTPE) and U is (F)arad, (H)enry, or (R) for ohms";
+	}
+
+	if ((defined($self->{value}) && !defined($self->{value_unit})) || 
+		(!defined($self->{value}) && defined($self->{value_unit})))
+	{
+		croak "value must be defined when value_unit is defined, and vice-versa.";
+	}
 
 	return $self;
 }
@@ -31,25 +93,21 @@ sub z0
 
 	my $z = $self->{z}[$port-1];
 
-	die "z0 is not defined for port $port" if (!defined $z);
+	croak "z0 is not defined for port $port" if (!defined $z);
 
 	return $z;
 }
 
-sub n_ports
-{
-	return shift->{n_ports};
-}
+sub n_ports { return shift->{n_ports}; }
 
-sub comments
-{
-	return @{ shift->{comments} // [] };
-}
+sub model { return shift->{model}; }
 
-sub measurements
-{
-	return @{ shift->{measurements} // [] };
-}
+sub value { return shift->{value}; }
+sub value_unit { return shift->{value_unit}; }
+
+sub comments { return @{ shift->{comments} // [] }; }
+
+sub measurements { return @{ shift->{measurements} // [] }; }
 
 
 sub get_measurement
@@ -73,7 +131,7 @@ sub get_measurement
 	# Exact match, return this one:
 	return $cur if ($cur->hz == $hz);
 
-	die "unable to find Hz for evaluation at $hz" if (!defined $prev);
+	croak "unable to find Hz for evaluation at $hz" if (!defined $prev);
 
 	my $prev_hz = $prev->hz;
 	my $cur_hz = $cur->hz;
@@ -93,5 +151,55 @@ sub get_measurement
 	# and make sure it is the same class type because
 	# 'params' could be S-, Y-, Z-params, etc.
 	return $cur->clone(ref($cur), params => $ret);
+}
+
+sub _parse_model_value_code
+{
+	my ($model, $regex) = @_;
+
+	my $val;
+	if ($model =~ /$regex/i && $1)
+	{
+		$val = $1;
+	}
+	else
+	{
+		carp "value_code_regex does not match: $model !~ $regex";
+		return undef;
+	}
+
+	# Decimal point: 1R3 = 1.3 Ohms, 1N3 = 1.3 nH, etc.
+	if ( $val =~ s/[A-Z]/./i )
+	{
+		# These are strings, so put leading/trailing zeros at the decimal:
+		$val =~ s/^\./0./;
+		$val =~ s/\.$/.0/;
+
+		# Could be a string, so make it a float:
+		$val *= 1.0;
+	}
+	elsif ( $val =~ s/^(\d+)(\d)$/$1/ )
+	{
+		$val = $1 * (10 ** $2);
+	}
+	else
+	{
+		croak("$model: Value code could not be determined: '$val'");
+	}
+
+	return $val;
+}
+
+sub _parse_model_value_literal
+{
+	my ($model, $regex) = @_;
+	my $val;
+
+	if ( $model =~ /$regex/i && $1 )
+	{
+		$val = $1;
+	}
+
+	return $val;
 }
 
